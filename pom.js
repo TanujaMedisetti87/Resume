d60b9df66d43029e2d26aa31de566d4139357e56
Tanuja <TanujaMedisetti87@users.noreply.github.com>
2018-02-23 16:19:14 +0530

    Create pom.js

---
+++ pom.js
@@ -0,0 +1,23 @@
+var deployer = require('nexus-deployer');
+ 
+var release = {
+    groupId: 'ResumeTest',
+    artifactId: 'ResumeTest',
+    version: '1.0',
+    packaging: 'JAR',
+    auth: {
+      username:'admin',
+      password:'admin123'
+    },
+    pomDir: 'build',
+    url: 'http://ec2-54-209-206-172.compute-1.amazonaws.com:8081/nexus/content/repositories/releases/',
+    artifact: 'my-app.zip',
+    noproxy: 'http://ec2-54-209-206-172.compute-1.amazonaws.com',
+    cwd: ''
+};
+ 
+ 
+deployer.deploy(release, function(){
+    // your async call back here 
+    // done(); 
+});
